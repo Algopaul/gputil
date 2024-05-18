0406f324d319860c250c85cd0a4cb2e739679ad2
Algopaul <72927083+Algopaul@users.noreply.github.com>
2024-05-18 13:32:16 -0400

    enforce style and add docstring

--- gputil/__init__.py
+++ gputil/__init__.py
@@ -1,26 +1,23 @@
+"""gputil"""
 import numpy as np
 
 
-def write_matrix_binary(filename, A, x=None, y=None):
+def write_matrix_binary(filename, matrix, x=None, y=None):
   if x is None:
-    x = np.linspace(0, 1, A.shape[1])
+    x = np.linspace(0, 1, matrix.shape[1])
   if y is None:
-    y = np.linspace(0, 1, A.shape[0])
-  if filename is None:
-    filename='test.dat'
-  M = np.zeros((A.shape[0]+1, A.shape[1]+1))
-  M[0] = len(x)
-  M[0, 1:] = x
-  M[1:, 0] = y
-  M[1:, 1:] = A
+    y = np.linspace(0, 1, matrix.shape[0])
+  gnuplot_matrix = np.zeros((matrix.shape[0] + 1, matrix.shape[1] + 1))
+  gnuplot_matrix[0] = len(x)
+  gnuplot_matrix[0, 1:] = x
+  gnuplot_matrix[1:, 0] = y
+  gnuplot_matrix[1:, 1:] = matrix
   with open(filename, 'wb') as file:
-    M.astype('float32').tofile(file)
+    gnuplot_matrix.astype('float32').tofile(file)
 
 
-if __name__ == "__main__":
-  n, m=50,50
-  A=np.random.random((n,m))
-  print(A.shape)
-  x=np.linspace(0, 20, m)
-  y=np.linspace(0, 1, n)
-  write_matrix_binary('test.dat', A)
+if __name__ == '__main__':
+  n, m = 50, 80
+  test_matrix = np.random.random((n, m))
+  print(test_matrix.shape)
+  write_matrix_binary('test.dat', test_matrix)
